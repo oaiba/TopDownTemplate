e4f7184eed9174314e91dfadb2177c8281f31644
Bá Oai <baoai25.work@gmail.com>
2025-10-06 07:20:17 +0700

    init Project

---
+++ Source/TopDownTemplate/TopDownTemplate.Build.cs
@@ -0,0 +1,46 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TopDownTemplate : ModuleRules
+{
+	public TopDownTemplate(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"NavigationSystem",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"Niagara",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"TopDownTemplate",
+			"TopDownTemplate/Variant_Strategy",
+			"TopDownTemplate/Variant_Strategy/UI",
+			"TopDownTemplate/Variant_TwinStick",
+			"TopDownTemplate/Variant_TwinStick/AI",
+			"TopDownTemplate/Variant_TwinStick/Gameplay",
+			"TopDownTemplate/Variant_TwinStick/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/TopDownTemplate/TopDownTemplateGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TopDownTemplateGameMode.h"
+
+ATopDownTemplateGameMode::ATopDownTemplateGameMode()
+{
+	// stub
+}
